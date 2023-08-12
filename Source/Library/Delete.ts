96b14fff45d80d69adb9e8e66ca5c133c1f71a7d
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-12 11:40:01 +0300

    squash!

--- Source/Library/Delete.ts
+++ Source/Library/Delete.ts
@@ -19,21 +19,19 @@ export default async (
 	Header["X-Auth-Email"] = Email ?? Header["X-Auth-Email"];
 	Header["X-Auth-Key"] = Key ?? Header["X-Auth-Key"];
 
-	return (await _Project(ID, Header)).forEach((Project) => {
-		_Deployment(ID, Project.name, Header).then((Deployment) =>
-			Deployment.splice(0, Limit).forEach((Deployment) =>
-				fetch(
-					`${`https://api.cloudflare.com/client/v4/accounts/${ID}/pages/projects/${Project.name}/deployments`}/${
-						Deployment.id
-					}`,
-					{
-						method: "DELETE",
-						headers: Header,
-					}
-				).then((Response) => {
-					console.log(Response);
-				})
-			)
-		);
-	});
+	for (const Project of await _Project(ID, Header)) {
+		for (const Deployment of (
+			await _Deployment(ID, Project.name, Header)
+		).splice(0, Limit)) {
+			await fetch(
+				`${`https://api.cloudflare.com/client/v4/accounts/${ID}/pages/projects/${Project.name}/deployments`}/${
+					Deployment.id
+				}`,
+				{
+					method: "DELETE",
+					headers: Header,
+				}
+			);
+		}
+	}
 };
