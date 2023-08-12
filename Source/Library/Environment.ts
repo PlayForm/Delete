1a3ffe47ee43d644e19f86db30f9eb5ae2238679
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-12 05:57:25 +0300

    squash!

---
+++ Source/Library/Environment.ts
@@ -0,0 +1,12 @@
+import * as dotenv from "dotenv";
+import { z } from "zod";
+
+dotenv.config();
+
+export default z
+	.object({
+		ACCOUNT_EMAIL: z.string().default(""),
+		API_KEY: z.string().default(""),
+		ACCOUNT_ID: z.string().default(""),
+	})
+	.parse(process.env);
