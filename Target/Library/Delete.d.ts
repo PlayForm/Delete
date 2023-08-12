fd0b64e2a8e4f51994bbed535a2e2951dcc79c69
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-12 11:06:13 +0300

    squash!

--- Target/Library/Delete.d.ts
+++ Target/Library/Delete.d.ts
@@ -1,2 +1,7 @@
+export declare const Header: {
+    "content-type": string;
+    "X-Auth-Email": string;
+    "X-Auth-Key": string;
+};
 declare const _default: (Email?: string, ID?: string, Key?: string) => Promise<void>;
 export default _default;
