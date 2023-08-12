f82b271d53d42790a0ebe813fac94f45ba0e4980
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-12 12:26:40 +0300

    squash!

--- Source/Library/Delete.ts
+++ Source/Library/Delete.ts
@@ -2,15 +2,26 @@ import Deployment from "../Library/Deployment.js";
 import Environment from "../Library/Environment.js";
 import Project from "../Library/Project.js";
 
-const Days = 7;
-const Limit = 40;
-
 export const Header = {
 	"content-type": "application/json;charset=UTF-8",
 	"X-Auth-Email": Environment.Email,
 	"X-Auth-Key": Environment.Key,
 };
 
+export const Days = 7;
+export const Limit = 40;
+
+/**
+ * The Delete function deletes all deployments associated with a specific project ID using the
+ * Cloudflare API.
+ * @param Email - The `Email` parameter is the email address associated with the Cloudflare account. It
+ * is used to authenticate the API request.
+ * @param Key - The `Key` parameter is the authentication key used to access the Cloudflare API. It is
+ * used to authenticate the request and verify the identity of the user making the request.
+ * @param ID - The ID parameter represents the ID of the Cloudflare account. It is used to identify the
+ * account for which the deployments need to be deleted.
+ * @returns The function `Delete` returns an array of IDs that have been deleted.
+ */
 export default async (
 	Email = Environment.Email,
 	Key = Environment.Key,
