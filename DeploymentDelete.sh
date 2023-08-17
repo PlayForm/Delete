f04b6fc89270f37d2e2d9bf3b96f914ead30d6f2
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-17 22:36:09 +0300

    squash!

--- DeploymentDelete.sh
+++ DeploymentDelete.sh
@@ -8,23 +8,23 @@ mapfile -t Projects < <(curl -s --header "content-type: application/json;charset
 
 # Clean entries
 for i in "${!Projects[@]}"; do
-    Projects[i]=$(echo "${Projects[i]}" | sed ':a;N;$!ba;s/\n//g')
+	Projects[i]=$(echo "${Projects[i]}" | sed ':a;N;$!ba;s/\n//g')
 done
 
 # Fill entries
 for Project in "${Projects[@]}"; do
-    readarray -t Deployments < <(curl -s --header "content-type: application/json;charset=UTF-8" --header "X-Auth-Email: ${Email}" --header "X-Auth-Key: ${Key}" "https://api.cloudflare.com/client/v4/accounts/${ID}/pages/projects/${Project}/deployments" | jq -r .result[].id)
+	readarray -t Deployments < <(curl -s --header "content-type: application/json;charset=UTF-8" --header "X-Auth-Email: ${Email}" --header "X-Auth-Key: ${Key}" "https://api.cloudflare.com/client/v4/accounts/${ID}/pages/projects/${Project}/deployments" | jq -r .result[].id)
 
-    # Clean entries
-    for i in "${!Deployments[@]}"; do
-        Deployments[i]=$(echo "${Deployments[i]}" | sed ':a;N;$!ba;s/\n//g')
-    done
+	# Clean entries
+	for i in "${!Deployments[@]}"; do
+		Deployments[i]=$(echo "${Deployments[i]}" | sed ':a;N;$!ba;s/\n//g')
+	done
 
-    # Delete deployments
-    for Deployment in "${Deployments[@]}"; do
-        echo -e "$Project"
-        echo -e "$Deployment"
+	# Delete deployments
+	for Deployment in "${Deployments[@]}"; do
+		echo -e "$Project"
+		echo -e "$Deployment"
 
-        curl -s --header "content-type: application/json;charset=UTF-8" --header "X-Auth-Email: ${Email}" --header "X-Auth-Key: ${Key}" "https://api.cloudflare.com/client/v4/accounts/${ID}/pages/projects/${Project}/deployments/${Deployment}" | jq -r .success
-    done
+		curl -s --header "content-type: application/json;charset=UTF-8" --header "X-Auth-Email: ${Email}" --header "X-Auth-Key: ${Key}" "https://api.cloudflare.com/client/v4/accounts/${ID}/pages/projects/${Project}/deployments/${Deployment}" | jq -r .success
+	done
 done
--- Target/Worker.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="@cloudflare/workers-types/experimental" />
-export interface Env {
-    Email?: string;
-    ID?: string;
-    Key?: string;
-}
-declare const _default: ExportedHandler<Env, unknown, unknown>;
-export default _default;
