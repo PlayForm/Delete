b9c9b72ad1edfb22467ebcd861fc3630570e6c10
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-06 00:16:25 +0300

    squash!

--- Target/Library/Environment.js
+++ Target/Library/Environment.js
@@ -1,1 +1,1 @@
-import*as e from"dotenv";import{z as t}from"zod";e.config();const n=t.object({Email:t.string().default(""),ID:t.string().default(""),Key:t.string().default("")});export{n as Function};
+import*as e from"dotenv";import{z as t}from"zod";e.config();const r=t.object({Email:t.string().default(""),ID:t.string().default(""),Key:t.string().default("")});export{r as Fn};
