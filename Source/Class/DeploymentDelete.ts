c5efaefd35da29be20421a4c7432036a37a0d748
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-26 16:02:19 +0300

    squash!

--- Source/Class/DeploymentDelete.ts
+++ Source/Class/DeploymentDelete.ts
@@ -1,18 +1,21 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
-import Delete from "../Function/Delete.js";
-
 try {
-	new Command()
+	new (await import("commander")).Command()
 		.name("DeploymentDelete")
 		.version(process.env["VERSION_PACKAGE"] ?? "0.0.1")
 		.description("Deletes old deployments in your Cloudflare account.")
 		.option("-e, --Email <Email>", "Cloudflare Account E-mail.")
 		.option("-i, --ID <ID>", "Cloudflare Account ID.")
 		.option("-k, --Key <Key>", "Cloudflare API key.")
-		.action(async (Option) =>
-			console.log(await Delete(Option?.Email, Option?.ID, Option?.Key))
+		.action(async ({ Email, ID, Key }) =>
+			console.log(
+				await (
+					await import("../Function/Delete.js")
+				).default({ Email, ID, Key })
+			)
 		)
 		.parse();
 } catch (_Error) {}
+
+export {};
