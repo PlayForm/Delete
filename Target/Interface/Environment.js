bec6faace88677ff760eef3e741b0a8d497f5b23
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-28 23:29:59 +0300

    squash!

--- Target/Interface/Environment.js
+++ Target/Interface/Environment.js
@@ -1,1 +1,3 @@
-const{default:e}=await import("../Object/Environment.js");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Environment = (await Promise.resolve().then(function () { return require("../Object/Environment.js"); })).default;
--- Target/Object/Environment.js
+++ Target/Object/Environment.js
@@ -1,1 +1,10 @@
-(await import("dotenv")).config();const{string:t}=await import("zod");var a=(await import("zod")).object({Email:t().optional().default(""),ID:t().optional().default(""),Key:t().optional().default("")});export{a as default,t as string};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.string = void 0;
+(await Promise.resolve().then(function () { return require("dotenv"); })).config();
+exports.string = (await Promise.resolve().then(function () { return require("zod"); })).string;
+exports.default = (await Promise.resolve().then(function () { return require("zod"); })).object({
+    Email: (0, exports.string)().optional().default(""),
+    ID: (0, exports.string)().optional().default(""),
+    Key: (0, exports.string)().optional().default(""),
+});
